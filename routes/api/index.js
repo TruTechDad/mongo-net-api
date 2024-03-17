092af3eb77a19a3fd6fe0120f78576a48d512358
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2024-03-16 23:15:30 -0400

    based code off of module 18-25

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
-const videoRoutes = require('./videoRoutes');
 const userRoutes = require('./userRoutes');
 
-router.use('/videos', videoRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
