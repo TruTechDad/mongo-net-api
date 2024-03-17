5a91256625ba9099eb4d1b957ea26e36919d73bf
TruTechDad <78445381+TruTechDad@users.noreply.github.com>
2024-03-17 10:26:09 -0400

    added the thought.js for models

---
+++ models/thought.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdAtVal => dateFormat(createdAtVal)
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [ReactionSchema]
+},
+{
+  toJSON: {
+    getters: true,
+    virtuals:true,
+  },
+  id: false,
+}
+);
+
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
